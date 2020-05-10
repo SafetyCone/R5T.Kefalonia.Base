eacef38f667795f7b81b55cb1e714b5a6076f575
MinexAutomation <minex.automation@gmail.com>
2020-05-09 17:17:27 -0700

    Added IVisualStudioProjectFileSerializer service.

---
+++ source/R5T.Kefalonia.Base/Code/Services/Definitions/IVisualStudioProjectFileSerializer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+using R5T.Gloucester.Types;
+using R5T.Megara;
+
+
+namespace R5T.Kefalonia
+{
+    public interface IVisualStudioProjectFileSerializer : IFileSerializer<ProjectFile>
+    {
+    }
+}
