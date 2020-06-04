2ba80ce781a02ffa04cf17189701daefe99b36b7
MinexAutomation <minex.automation@gmail.com>
2020-06-03 21:50:14 -0700

    Added IVisualStudioProjectFileStreamSerializer service definition interface type.

---
+++ source/R5T.Kefalonia.Base/Code/Services/Definitions/IVisualStudioProjectFileStreamSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+using R5T.Gloucester.Types;
+
+
+namespace R5T.Kefalonia
+{
+    public interface IVisualStudioProjectFileStreamSerializer
+    {
+        /// <summary>
+        /// Includes both <paramref name="stream"/> and <paramref name="projectFilePath"/> since project reference relative file paths will need to be made absolute.
+        /// </summary>
+        Task<ProjectFile> DeserializeAsync(Stream stream, string projectFilePath);
+
+        /// <summary>
+        /// Includes both <paramref name="stream"/> and <paramref name="projectFilePath"/> since project reference absolute file paths will need to be made relative.
+        /// </summary>
+        Task SerializeAsync(Stream stream, string projectFilePath, ProjectFile value);
+    }
+}
